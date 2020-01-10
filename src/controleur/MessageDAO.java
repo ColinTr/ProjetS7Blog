8701402428fd8b0f12964543b097f734388dad80
Colin Troisemaine <56585263+ColinTr@users.noreply.github.com>
2020-01-10 11:43:51 +0100

    Fonctions de base à implémenter

--- src/controleur/MessageDAO.java
+++ src/controleur/MessageDAO.java
@@ -1,4 +1,37 @@
 package controleur;
 
+import modele.Message;
+import modele.MotCle;
+
+import java.util.List;
+
 public class MessageDAO {
+
+    public static boolean posterMessage(){
+        //TODO
+        return true;
+    }
+
+    public static boolean supprimerMessage(){
+        //TODO
+        return true;
+    }
+
+    public static boolean modifierMessage(){
+        //TODO
+        return true;
+    }
+
+    public static List<Message> recupererTousLesMessages(){
+        return null;
+    }
+
+    public static List<Message> recupererMessagesAUneDateDonnee(java.util.Date date){
+        return null;
+    }
+
+    public static List<Message> recupererMessagesAvecMotsCles(List<MotCle> motCles){
+        return null;
+    }
+
 }
