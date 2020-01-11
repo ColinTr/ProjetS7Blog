b44e696130d0012d16f149971ab87027c3b5ca8f
ColinTr <56585263+ColinTr@users.noreply.github.com>
2020-01-11 15:54:39 +0100

    Création d'un main de génération de bdd de test

--- src/vue/Main.java
+++ src/vue/Main.java
@@ -3,6 +3,7 @@ package vue;
 import controleur.Connexion;
 import controleur.ManipulationUtilisateur;
 import controleur.UtilisateurDAO;
+import modele.Message;
 import modele.Utilisateur;
 
 /**
@@ -17,6 +18,28 @@ public class Main {
         Utilisateur azer = new Utilisateur("Nom", "Prenom", "10 rue olivier debré", "azer", UtilisateurDAO.SHA512("azer"));
         Utilisateur colin = new Utilisateur("Troisemaine", "Colin", "10 rue des peupliers", "colin.troisemaine@etu.univ-tours.fr", UtilisateurDAO.SHA512("password"));
         Utilisateur guillaume = new Utilisateur("Bouchard", "Guillaume", "64 Avenue Jean Portalis", "guillaume.bouchard@etu.univ-tours.fr", UtilisateurDAO.SHA512("password"));
+
+        String loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+        Message m1 = new Message("titre message 1", loremIpsum, new java.sql.Date(System.currentTimeMillis()));
+        Message m2 = new Message("titre message 2", loremIpsum, new java.sql.Date(System.currentTimeMillis()));
+        Message m3 = new Message("titre message 3", loremIpsum, new java.sql.Date(System.currentTimeMillis()));
+        Message m4 = new Message("titre message 4", loremIpsum, new java.sql.Date(System.currentTimeMillis()));
+        Message m5 = new Message("titre message 5", loremIpsum, new java.sql.Date(System.currentTimeMillis()));
+
+
+
+        m1.setUtilisateur(azer);
+        m2.setUtilisateur(colin);
+        m3.setUtilisateur(guillaume);
+        m4.setUtilisateur(azer);
+        m5.setUtilisateur(colin);
+
+        Connexion.getEntityManager().persist(m1);
+        Connexion.getEntityManager().persist(m2);
+        Connexion.getEntityManager().persist(m3);
+        Connexion.getEntityManager().persist(m4);
+        Connexion.getEntityManager().persist(m5);
 
         Connexion.getEntityManager().persist(azer);
         Connexion.getEntityManager().persist(colin);
