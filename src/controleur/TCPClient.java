df7a81f2eb16e017969d9ba6c2736426df763e47
GuillaumeBchd <guillaumebouchard@orange.fr>
2020-01-12 21:47:58 +0100

    Ajout des classes TCP pour le transfert d'image du client au serveur.

---
+++ src/controleur/TCPClient.java
@@ -0,0 +1,78 @@
+package controleur;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.file.Paths;
+
+public class TCPClient {
+
+    private final static String serverIP = "127.0.0.1";
+    private final static int serverPort = 4444;
+
+    public static void main( String[] args ) {
+        System.out.println(TCPClient.uploadImage("C:\\Users\\GuillaumeBouchard\\Desktop\\a.jpg"));
+    }
+
+    public static String uploadImage(String chemin){
+
+        // On va définir un protocole qui est le suivant :
+        // - 4 premier bit définisse la taille du nom en bit
+        // - nom de l'image avec extension
+        // - taille du fichier sur 8 bits
+        // - données du fichier
+
+        Socket socket = null;
+        InputStream in = null;
+        OutputStream out = null;
+
+        byte[] buffer = new byte[4096];
+
+        try{
+            socket = new Socket(serverIP,serverPort);
+            in = socket.getInputStream();
+            out = socket.getOutputStream();
+
+            byte[] filenameArray = Paths.get(chemin).getFileName().toString().getBytes();
+            int filenameSize = filenameArray.length;
+
+            out.write(toByteArray(filenameSize));
+            out.write(filenameArray);
+            File file = new File(chemin);
+            out.write(toByteArray(file.length()));
+
+            FileInputStream fileInputStream = new FileInputStream(chemin);
+
+            int count;
+            while ((count = fileInputStream.read(buffer)) > 0 && count <= file.length())
+            {
+                out.write(buffer, 0, count);
+            }
+            fileInputStream.close();
+
+            String cheminServeur = new String(in.readAllBytes());
+
+            out.close();
+            in.close();
+            socket.close();
+
+            return cheminServeur;
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+    private static byte[] toByteArray(int value) {
+        return  ByteBuffer.allocate(4).putInt(value).array();
+    }
+
+    private static byte[] toByteArray(long value) {
+        return  ByteBuffer.allocate(8).putLong(value).array();
+    }
+
+}
---
+++ src/controleur/TCPServeur.java
@@ -0,0 +1,76 @@
+package controleur;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+public class TCPServeur {
+
+    private final static int serverPort = 4444;
+
+    public static void main( String[] args ) {
+        String rootDirectory = "C:\\Images\\";
+        TCPServeur.receiveImage(rootDirectory);
+    }
+
+    public static void receiveImage(String rootDirectory){
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        InputStream in = null;
+        OutputStream out = null;
+
+        byte[] buffer = new byte[4096];
+
+        try {
+            serverSocket = new ServerSocket(serverPort);
+            System.out.println("En attente de connexion...");
+            while (true){
+                socket = serverSocket.accept();
+                System.out.println("Connexion depuis "+socket.getInetAddress());
+
+                in = socket.getInputStream();
+                out = socket.getOutputStream();
+
+                in.readNBytes(buffer,0,4);
+                int filenameSize = fromByteArray(buffer);
+
+                in.readNBytes(buffer,0,filenameSize);
+                String filename = new String(buffer,0,filenameSize);
+
+                in.readNBytes(buffer,0,8);
+                long fileSize = longFromByteArray(buffer);
+
+                String path = (rootDirectory+filename);
+
+                FileOutputStream fileOutputStream = new FileOutputStream(path);
+                int count = 0;
+                int dataReceived = 0;
+                do {
+                    count = in.read(buffer);
+                    dataReceived += count;
+                    fileOutputStream.write(buffer, 0, count);
+                } while(count != -1 && dataReceived < fileSize);
+                fileOutputStream.close();
+
+                out.write(path.getBytes());
+
+                in.close();
+                out.close();
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static int fromByteArray(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getInt();
+    }
+
+    private static long longFromByteArray(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getLong();
+    }
+
+}
