d9d99d169b1962cf4c52c547f764e38eb6a2fc38
ColinTr <56585263+ColinTr@users.noreply.github.com>
2020-01-13 20:08:33 +0100

    Mise en place du modèle de données

--- src/controleur/ControleurDonnees.java
+++ src/controleur/ControleurDonnees.java
@@ -8,4 +8,6 @@ public class ControleurDonnees {
     public ControleurDonnees(ModeleDonnees modeleDonnes) {
         this.modeleDonnes = modeleDonnes;
     }
+
+
 }
--- src/modele/ModeleDonnees.java
+++ src/modele/ModeleDonnees.java
@@ -6,13 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ModeleDonnees {
-    private final Utilisateur utilisateurConnecte = null;
+    private final Utilisateur utilisateurConnecte;
     private final List<Message> messageList = new ArrayList<>();
     private final List<MotCle> motCleList = new ArrayList<>();
-    private final ObsFenetrePrincipale obsFenetrePrincipale = null; //On l'initialise à null, mais il sera rédfini une fois qu'une instance de ObsFenetrePrincipale est créée
+    private final ObsFenetrePrincipale obsFenetrePrincipale; //On l'initialise à null, mais il sera rédfini une fois qu'une instance de ObsFenetrePrincipale est créée
 
-    public void setUtilisateurConnecte(Utilisateur utilisateurConnecte){
+    public ModeleDonnees(Utilisateur utilisateurConnecte, ObsFenetrePrincipale obsFenetrePrincipale) {
         this.utilisateurConnecte = utilisateurConnecte;
+        this.obsFenetrePrincipale = obsFenetrePrincipale;
     }
 
     public Utilisateur getUtilisateurConnecte() {
@@ -29,9 +30,5 @@ public class ModeleDonnees {
 
     public ObsFenetrePrincipale getObsFenetrePrincipale() {
         return obsFenetrePrincipale;
-    }
-
-    public void setObsFenetrePrincipale(ObsFenetrePrincipale obsFenetrePrincipale){
-        this.obsFenetrePrincipale = obsFenetrePrincipale;
     }
 }
